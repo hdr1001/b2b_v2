6dc3ced007869e4f4cb3a3c730c90eda822fbc1f
hdr1001 <HansDeRooij@Hotmail.com>
2026-02-19 21:12:21 +0100

    Added navigation bar and accompanying CSS

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <p>Start here</p>
-  </div>
+  <header>
+    <div class="app-name">B2B v2</div>
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  </header>
+  <main>
+  </main>
 `;
