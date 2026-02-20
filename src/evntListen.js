9fa21b6ee2f4b2ae1c190c967f6d7d9cba0c3dc0
hdr1001 <HansDeRooij@Hotmail.com>
2026-02-20 20:46:36 +0100

    Added about dialog

---
+++ src/evntListen.js
@@ -0,0 +1,36 @@
+/* ********************************************************************
+//
+// Business-to-business (B2B) application v2
+// Application event listeners
+//
+// Copyright 2026 Hans de Rooij 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ***************************************************************** */
+
+function setupEventListeners() {
+    //HTML element references
+    const navAbout     = document.querySelector('#nav-about');
+    const navContact   = document.querySelector('#nav-contact');
+    const dialogAbout  = document.querySelector('#dialog-about');
+
+    //Navigation menu event listeners
+    navAbout.addEventListener('click', () => dialogAbout.showModal());
+    navContact.addEventListener('click', () => console.log('Contact clicked'));
+
+    //Dialog event listeners
+    dialogAbout.querySelector('#dialog-btn-close').addEventListener('click', () => dialogAbout.close());
+}
+
+export default setupEventListeners;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,52 @@
+/* ********************************************************************
+//
+// Business-to-business (B2B) application v2
+// Application entry point and main application logic
+//
+// Copyright 2026 Hans de Rooij 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ***************************************************************** */
 import './style.css';
+import setupEventListeners from './evntListen.js';
 
+console.log('Top of main.js');
+
+//Main application structure
 document.querySelector('#app').innerHTML = `
   <header>
     <div class="app-name">B2B v2</div>
     <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#contact">Contact</a>
+      <a href="javascript:void(0)">Home</a>
+      <a href="javascript:void(0)" id="nav-about">About</a>
+      <a href="javascript:void(0)" id="nav-contact">Contact</a>
     </nav>
   </header>
-  <main>
-  </main>
+  <main id="app-main"></main>
 `;
+
+//The about dialog
+document.querySelector('#app-main').innerHTML = `
+  <dialog id="dialog-about">
+    <h2>Welcome to B2B v2</h2>
+    <p>This is the main application for business-to-business interactions.</p>
+    <button id="dialog-btn-close">Close</button>
+  </dialog>
+`;
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM content loaded');
+
+    setupEventListeners();
+});
